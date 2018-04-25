24b137792413235f7f14ff82629e27bb4ce98fd9
Blake <blake.hawkins97@gmail.com>
2018-04-25 16:50:37 -0500

    Finished Exercise 2

--- Problem-Set-12/Risk.hs
+++ Problem-Set-12/Risk.hs
@@ -4,6 +4,8 @@ module Risk where
 
 import Control.Monad.Random
 import Data.Bifunctor (first)
+import Data.List
+import Data.Typeable
 
 ------------------------------------------------------------
 -- Die values
@@ -23,10 +25,18 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+  deriving (Show)
 
 -- Exercise 2
 
-atkDice :: Int -> Rand StdGen [Int]
-atkDice n = replicateM die n
+rolls :: Int -> Rand StdGen [DieValue]
+rolls n = replicateM n die
 
 battle :: Battlefield -> Rand StdGen Battlefield
+battle bf = do
+  atkRolls <- rolls (attackers bf)
+  defRolls <- rolls (defenders bf)
+  let adPairs  = zip (sort atkRolls) (sort defRolls)
+      atkLoss  = length (filter (\x -> snd x > fst x) adPairs)
+      defLoss  = length (filter (\x -> fst x > snd x) adPairs)
+  return (Battlefield ((attackers bf) - atkLoss) ((defenders bf) - defLoss))
